d181565166e5d3767aae74a9ddd17d64b14f519a
Billy Zhang <45053514+Dx-B@users.noreply.github.com>
2026-05-18 20:51:12 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Hashtable<Integer,Integer> table = new Hashtable<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (table.containsKey(nums[i])==false) {
+                table.put(nums[i],1);
+            }
+            else {
+                return true;
+            }
+        }
+        System.gc();
+        return false;
+    }
+}
