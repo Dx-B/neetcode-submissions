80e59d616e588723d1f78402b47350bf66b179fc
Billy Zhang <45053514+Dx-B@users.noreply.github.com>
2026-06-03 22:23:24 -0400

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int low = Integer.MAX_VALUE;
+        int high = Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int c : prices) {
+            low = Math.min(c,low);
+            max = Math.max(max, c-low);
+        }
+        return max;
+    }
+}
