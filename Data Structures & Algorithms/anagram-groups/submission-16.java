72c1b230105d4064addea52a7926ecc7bfc58332
Billy Zhang <45053514+Dx-B@users.noreply.github.com>
2026-06-09 19:21:26 -0400

    Add: anagram-groups - submission-16

---
+++ Data Structures & Algorithms/anagram-groups/submission-16.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
+        for (String c : strs) {
+            char[] a = c.toCharArray();
+            Arrays.sort(a);
+            String sorted = new String(a);
+            if (!map.containsKey(sorted)) {
+                map.put(sorted, new ArrayList<String>());
+            }
+            map.get(sorted).add(c);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
