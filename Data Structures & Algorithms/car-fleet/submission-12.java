eb72e0ead20e9f2b4d90beed14c5f07fc49521aa
Billy Zhang <45053514+Dx-B@users.noreply.github.com>
2026-06-12 21:05:09 -0400

    Add: car-fleet - submission-12

---
+++ Data Structures & Algorithms/car-fleet/submission-12.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int[][] cars = new int[position.length][2];
+        for (int i = 0; i < position.length; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = speed[i];
+        }
+        Arrays.sort(cars, (a,b) -> Integer.compare(b[0],a[0])); // sort position in descending order
+        Stack<Double> stack = new Stack<>();
+        for (int[] car : cars) {
+            double time = (double) (target-car[0])/car[1];
+            stack.push(time);
+            if (stack.size() >= 2 && stack.peek() <= stack.get(stack.size()-2)) {
+                stack.pop();
+            }
+        }
+        return stack.size();
+    }
+}
