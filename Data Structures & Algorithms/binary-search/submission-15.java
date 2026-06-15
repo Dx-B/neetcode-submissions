0f802dbc65d3f3014f5aa1f1a042dd9da60a2fab
Billy Zhang <45053514+Dx-B@users.noreply.github.com>
2026-06-14 23:14:46 -0400

    Add: binary-search - submission-15

---
+++ Data Structures & Algorithms/binary-search/submission-15.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+        while (l <= r) {
+            int middle = (l+r)/2;
+            if (nums[middle] > target) {
+                r = middle-1;
+            }
+            else if (nums[middle] < target) {
+                l = middle+1;
+            }
+            else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+}
